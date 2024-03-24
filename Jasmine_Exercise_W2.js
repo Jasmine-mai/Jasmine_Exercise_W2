c30a862ae6dd1b65a7bd97878cf39d1111f4382e
Jasmine-mai <>
2024-03-24 15:45:37 +0700

    Jasmine update Do/ White

--- Jasmine_Exercise_W2.js
+++ Jasmine_Exercise_W2.js
@@ -22,4 +22,21 @@ for (var key in person){
     console.log (person[key]);
 }
 
-
+// Exercise 2
+// Vòng lặp Do/ White
+var i = 0;
+do {
+    i++;
+    console.log(i);
+} while (i < 10);
+// Sự khác biệt với for: k kiểm tra điều kiện ở lần đầu tiên, ktra từ lần 2
+// Sử dụng vòng lặp While trong tình huống người dùng login không thành công và có thể login lại 5 lần
+var i = 0
+var isSuccess = false;
+do {
+    i++;
+    console.log('số lần login ' + i);
+    if (false) {
+        isSuccess = true;
+    }
+} while (!isSuccess && i <= 5);
